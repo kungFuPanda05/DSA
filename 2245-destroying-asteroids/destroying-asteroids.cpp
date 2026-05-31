b78327a0da8b12ecd9b4844d6428ea7c85b5c239
kungFuPanda05 <vishal7920821202@gmail.com>
2026-06-01 00:56:06 +0530

    Time: 338 ms (5.84%) | Memory: 183.5 MB (5.11%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int Mass, vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-
-        long long mass = Mass;
-
-        for (int asteroid : arr) {
-            if (asteroid > mass) {
-                return false;
-            }
-            mass += asteroid;
+        multiset<long long> st;
+        for(auto val: arr){
+            st.insert(val);
         }
-
+        long long mass = Mass;
+        while(st.size()){
+            auto it = st.upper_bound(mass);
+            if(it==st.begin()) return false;
+            it--;
+            mass += *it;
+            st.erase(it);
+        }
         return true;
     }
 };
