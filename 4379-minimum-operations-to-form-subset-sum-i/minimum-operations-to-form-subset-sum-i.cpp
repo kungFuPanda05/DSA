baac152e0b99a35fcb6876687345a6df84600b31
kungFuPanda05 <vishal7920821202@gmail.com>
2026-09-03 16:29:09 +0530

    Time: 896 ms (8.14%) | Memory: 67.2 MB (56.73%) - LeetSync

---
+++ 4379-minimum-operations-to-form-subset-sum-i/minimum-operations-to-form-subset-sum-i.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector <int> arr;
+    int n;
+    const int inf = INT_MAX-100;
+    vector <vector <int>> dp;
+    int f(int i, int sum){
+
+        if(sum==0) return 0;
+        if(i==n || sum<0) return inf;
+        if(dp[i][sum]!=-1) return dp[i][sum];
+
+        int num = arr[i];
+        int ans1 = f(i+1, sum);
+        int op = 0;
+
+        while(num<=sum){
+            ans1 = min(ans1, op+f(i+1, sum-num));
+            num *= 2;
+            op++;
+        }
+
+        num = arr[i];
+        op = 0;
+        int ans2 = f(i+1, sum);
+        while(num>0){
+            ans2 = min(ans2, op+f(i+1, sum-num));
+            num /= 2;
+            op++;
+        }
+        return dp[i][sum] = min({ans1, ans2, inf});
+    }
+
+    int minOperations(vector<int>& nums, int sum) {
+        arr = nums;
+        n = arr.size();
+        dp = vector <vector <int>> (n+1, vector <int> (sum+1, -1));
+        int ans = f(0, sum);
+        if(ans==inf) return -1;
+        return ans;
+    }
+};
